ee8ac7eed9d90d34ff17cb27e8228aaffb44633a
Renato Ferraz <rfcbf@me.com>
2018-12-13 16:25:50 -0300

    no message

--- src/main/java/br/com/esig/todo/controller/RestTodoController.java
+++ src/main/java/br/com/esig/todo/controller/RestTodoController.java
@@ -1,15 +1,12 @@
 package br.com.esig.todo.controller;
 
-import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 import br.com.esig.todo.entity.Todo;
 import br.com.esig.todo.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class RestTodoController {
@@ -27,15 +24,13 @@ public class RestTodoController {
         todoService.excluir(id);
     }
 
-    @PostMapping("/api")
-    public ResponseEntity<Object> salvar(@RequestBody Todo todo) {
-        Todo todos = todoService.salvar(todo);
 
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-                .buildAndExpand(todo.getId()).toUri();
-
-        return ResponseEntity.created(location).build();
-
+    @PostMapping("/api/{texto}")
+    public void salvar(@PathVariable String texto) {
+        Todo todo = new Todo();
+        todo.setConcluido(false);
+        todo.setTexto(texto);
+        todoService.salvar(todo);
     }
 
     @PutMapping("/api")
